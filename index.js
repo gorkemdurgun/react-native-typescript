c02e61b15621896183a9551b8f5b86faf061d7a6
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-08-16 23:39:20 +0300

    first redux example

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import {name as appName} from './app.json';
 import { Counter } from './components/counter/CounterFunction';
 import { Dog } from './components/reducer/Dog';
 import Home from './src/Home';
+import Main from './src/ReduxExample/Main';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => Main);
